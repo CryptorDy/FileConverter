c39342d6803bd2dff42f6b178b3466af30943fc5
Bagaev <ib@bagaev.com>
2025-07-30 14:21:32 +0300

    Миграция AudioAnalysis

--- FileConverter/Migrations/20250730112119_addAudioAnalysis.Designer.cs
+++ FileConverter/Migrations/20250730112119_addAudioAnalysis.Designer.cs
@@ -5,6 +5,7 @@ using FileConverter.Data;
 using FileConverter.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FileConverter.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250730112119_addAudioAnalysis")]
+    partial class addAudioAnalysis
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FileConverter/Migrations/20250730112119_addAudioAnalysis.cs
@@ -0,0 +1,39 @@
+﻿using FileConverter.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileConverter.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAudioAnalysis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<AudioAnalysisData>(
+                name: "AudioAnalysis",
+                table: "MediaItems",
+                type: "jsonb",
+                nullable: true);
+
+            migrationBuilder.AddColumn<AudioAnalysisData>(
+                name: "AudioAnalysis",
+                table: "ConversionJobs",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AudioAnalysis",
+                table: "MediaItems");
+
+            migrationBuilder.DropColumn(
+                name: "AudioAnalysis",
+                table: "ConversionJobs");
+        }
+    }
+}
